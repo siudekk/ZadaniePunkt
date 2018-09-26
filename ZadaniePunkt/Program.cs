16f2d00838b7264a82fbc875d6eed36ebe7cc6ba
Tomasz Dawid <tomasz.dawid@balluff.pl>
2018-09-26 10:00:08 +0200

    Dodaj pliki projektów.

---
+++ ZadaniePunkt/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaniePunkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x1;
+            int x2;
+
+            string y1;
+            int y2;
+
+
+            Punkt XY = new Punkt(5,5); // 2) Współrzędne punktu są ustalane na etapie tworzenia obiektu (konstruktor)
+
+            XY.Wyswietl();
+
+            Console.WriteLine("Przesuń w prawo o:");
+            x1 = Console.ReadLine();
+            x2 = System.Int32.Parse(x1);
+            XY.Prawo(x2);
+
+            Console.WriteLine("Przesuń w lewo o:");
+            x1 = Console.ReadLine();
+            x2 = System.Int32.Parse(x1);
+            XY.Lewo(x2);
+
+            Console.WriteLine("Przesuń w górę o:");
+            y1 = Console.ReadLine();
+            y2 = System.Int32.Parse(y1);
+            XY.Gora(y2);
+
+            Console.WriteLine("Przesuń w dół o:");
+            y1 = Console.ReadLine();
+            y2 = System.Int32.Parse(y1);
+            XY.Dol(y2);
+
+
+            XY.Wyswietl();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ZadaniePunkt/Punkt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaniePunkt
+{
+    class Punkt
+    {
+
+        public int X { get; private set; }// właściwość 1
+        
+
+        public int Y { get; private set; }// właściwość 2
+       
+
+
+        public Punkt(int wspx, int wspy) //konstruktor
+        {
+            X = wspx;
+            Y = wspy;
+
+        }
+
+        public void Prawo(int ileP) //metoda 1
+        {
+            X += ileP;
+
+        }
+
+        public void Lewo(int ileL) //metoda 2
+        {
+            X -= ileL;
+
+        }
+
+        public void Gora(int ileG) //metoda 3
+        {
+            Y += ileG;
+
+        }
+
+        public void Dol(int ileD) //metoda 4
+        {
+            Y -= ileD;
+
+        }
+
+
+        public void Wyswietl()
+        {
+
+            Console.WriteLine("Współrzędna X: {0}",X);
+            Console.WriteLine("Współrzędna Y: {0}",Y);
+        }
+
+    }
+}
